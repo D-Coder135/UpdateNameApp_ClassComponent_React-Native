9b0ddfbd8c1d08e88b0088b1fe2439db59cb61dc
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-10 22:46:19 +0530

    add a View tag inside the return statement of the render() function

--- components/UpdateNameUsingClass.js
+++ components/UpdateNameUsingClass.js
@@ -2,9 +2,11 @@ import * as React from 'react';
 import { Text, View, StyleSheet, Button, TextInput } from 'react-native';
 
 export default class UpdateNameUsingClass extends React.Component {
-    render(
+    render() {
         return(
-            
+            <View>
+                
+            </View>
         );
-    );
+    }
 }
