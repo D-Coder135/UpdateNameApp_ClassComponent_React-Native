64d2a301df489ecac47643b4f589cf152dbfa86e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-10 22:42:19 +0530

    add a render() function inside the class UpdateNameUsingClass

--- components/UpdateNameUsingClass.js
+++ components/UpdateNameUsingClass.js
@@ -2,5 +2,7 @@ import * as React from 'react';
 import { Text, View, StyleSheet, Button, TextInput } from 'react-native';
 
 export default class UpdateNameUsingClass extends React.Component {
-    
+    render(
+        
+    );
 }
