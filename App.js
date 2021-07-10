5e9f060513b5f0451ac9b7132cc4e411579ccecb
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-10 22:33:28 +0530

    add a function App() along with the export default

--- App.js
+++ App.js
@@ -3,3 +3,7 @@ import {Text, View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 import UpdateNameUsingClass from './components/UpdateNameUsingClass';
 import {Card} from 'react-native-paper';
+
+export default function App() {
+  
+}
