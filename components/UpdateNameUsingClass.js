892d86113a49b8322559c4d8c31ae2f4a3d51548
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-10 22:47:51 +0530

    add the const styles field along with some attribute inside it

--- components/UpdateNameUsingClass.js
+++ components/UpdateNameUsingClass.js
@@ -5,8 +5,33 @@ export default class UpdateNameUsingClass extends React.Component {
     render() {
         return(
             <View>
-                
+
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 24,
+    },
+    paragraph: {
+      margin: 24,
+      marginTop: 0,
+      fontSize: 14,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    logo: {
+      height: 128,
+      width: 128,
+    },
+    view: {
+        flexDirection: 'row',
+    },
+    inputText: {
+        borderWidth: 1.5, backgroundColor: 'white', padding: 15, marginTop: 15, marginBottom: 20, borderRadius: 10, textAlign: 'center', fontSize: 20
+      },
+  });
