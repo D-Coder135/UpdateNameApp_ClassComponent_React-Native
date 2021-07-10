17195a1ab8cb5b2684ae3a10340966277af6456a
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-10 22:35:34 +0530

    add the const styles field along with some attributes inside it

--- App.js
+++ App.js
@@ -6,6 +6,22 @@ import {Card} from 'react-native-paper';
 
 export default function App() {
   return(
-    
+
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 18,
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
